87257ba16122b5e62358e7e8e1bda603adaea89e
Phong Le <phongleq@gmail.com>
2019-12-11 15:01:35 -0700

    Added Carthage supporrt

---
+++ AssociatedValues/AssociatedValues.h
@@ -0,0 +1,19 @@
+//
+//  AssociatedValues.h
+//  AssociatedValues
+//
+//  Created by Phong Le on 12/11/19.
+//  Copyright © 2019 Brad Hilton. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AssociatedValues.
+FOUNDATION_EXPORT double AssociatedValuesVersionNumber;
+
+//! Project version string for AssociatedValues.
+FOUNDATION_EXPORT const unsigned char AssociatedValuesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AssociatedValues/PublicHeader.h>
+
+
